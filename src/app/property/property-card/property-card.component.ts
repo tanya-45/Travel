18d8f104b419f6dbc9865aac09c80df155cf7c74
tanya4558 <tanya501singh@gmail.com>
2022-04-13 14:10:25 +0530

    first commit

---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component ( {
+  selector: 'app-property-card',
+  templateUrl: 'property-card.component.html',
+  styleUrls: ['property-card.component.css']
+}
+)
+
+export class PropertyCardComponent {
+ Property: any = {
+   "id":1,
+   "Name": "Orange-INN",
+   "Type":"Hotel",
+   "Price":2000
+ }
+}
